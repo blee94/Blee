8e0fe789b3546dc7451170fc5ebdc192592dd4a0
blee94 <135qudwls@naver.com>
2023-10-10 18:17:23 +0900

    node module

--- node.js/index.js
+++ node.js/index.js
@@ -1,4 +1,6 @@
 // require: 모듈 ()파일을 불러온다.
+// 하나 이상의 식별자를 객체 형태로 받아오는 경우
+// 모듈을 받아올 때 객체를 그대로 math라는 식별자로 받음.
 
 const math = require("./math");
 
@@ -6,3 +8,9 @@ const sum = math.add(2, 3);
 const minus = math.minus(2, 3);
 
 console.log(sum, minus);
+
+// // 특정 식별자만 받아오고 싶은 경우
+// //모듈을 받아올 때 사용하고싶은 식별자만 {}내부에 추출한 이름 그대로 입력. ,로 여러개 받아 올수도 있음.
+// const { add } = require("./math");
+// const sum = math.add(5, 3);
+// console.log(sum);
--- node.js/math.js
+++ node.js/math.js
@@ -7,8 +7,17 @@ const minus = (a, b) => a - b;
 //   return a + b;
 // };
 
+// 한개의 식별자만 내보내는 경우
 // 이 모듈을 내보내서 다른 파일에서 사용할 수 있음.
+// module.exports = add;
+
+//함수 이름만 입력. 함수 자체를 내보내면 안됌. (add():x)
+//여러개의 식별자를 내보내는 경우
 module.exports = {
   add,
   minus,
-}; //함수 이름만 입력. 함수 자체를 내보내면 안됌.
+};
+
+//파일에서 여러가의 식별자를 객체 형태로 내보내는 경우
+// module.exports.add = (a,b) => a+b;
+// module.exports.minus = (a,b) => a-b;
