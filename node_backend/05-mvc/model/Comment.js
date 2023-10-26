9f41fb1ca8a4b892982eea14d0715926fbb2cdf1
blee94 <135qudwls@naver.com>
2023-10-26 18:36:57 +0900

    mvc

---
+++ node_backend/05-mvc/model/Comment.js
@@ -0,0 +1,16 @@
+exports.commentInfos = () => {
+  return [
+    {
+      id: 1,
+      userid: "blee",
+      date: "2023-10-26",
+      comment: "hello",
+    },
+    {
+      id: 2,
+      userid: "duly",
+      date: "2025-10-26",
+      comment: "hello world",
+    },
+  ];
+};
