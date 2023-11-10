358a4ed07e13413a3976fdc3cbb71092902e521b
blee94 <135qudwls@naver.com>
2023-11-11 02:44:27 +0900

    project_prototype

---
+++ project_prototype/controller/Cuser.js
@@ -0,0 +1,20 @@
+// const { User } = require('../model/index');
+
+// exports.index = (req, res) => {
+//   res.render('index');
+// };
+// exports.detail = (req, res) => {
+//   res.render('detail');
+// };
+// exports.signup = (req, res) => {
+//   res.render('signup');
+// };
+// exports.signin = (req, res) => {
+//   res.render('signin');
+// };
+// exports.findid = (req, res) => {
+//   res.render('findis');
+// };
+// exports.findpw = (req, res) => {
+//   res.render('findpw');
+// };
